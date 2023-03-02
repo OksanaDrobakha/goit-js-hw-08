e6d126094cbffb7d5b2983b4c9e55af27880d0c4
OksanaDrobakha <6249784@gmail.com>
2023-03-02 20:11:02 +0400

    hw 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const imgElements = galleryItems.reduce(
+  (acum, { preview, original, description }) =>
+    (acum += `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`),
+  ''
+);
+
+gallery.insertAdjacentHTML('beforeend', imgElements);
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
