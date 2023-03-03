77f956bfa3faf95dd55f3939971b5ad9c93ffc9e
OksanaDrobakha <6249784@gmail.com>
2023-03-03 21:29:34 +0400

    after fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,6 @@
-import { throttle } from 'lodash.throttle';
+import { throttle } from 'lodash';
 
-const form = document.querySelector('feedback-form');
+const form = document.querySelector('.feedback-form');
 const email = document.querySelector('input[name="email"]');
 const message = document.querySelector('textarea[name="message"]');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
